fc1c02c954ea11eba6426f98fa3e49f924fbc6e7
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-06-25 19:18:01 +0500

    config build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,25 @@
 plugins {
     id("java")
+    application
+    checkstyle
+    id ("com.github.ben-manes.versions") version "0.51.0"
+    id ("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.App"
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("info.picocli:picocli:4.7.6")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.6")
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
