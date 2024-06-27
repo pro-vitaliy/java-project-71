cc1cff03481fc55bd31f4427a3fd8b34e17acbac
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-06-27 23:18:54 +0500

    add coll() method to App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,12 +5,12 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-import java.io.File;
+import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 
-public class App {
+public class App implements Callable<String> {
 
     @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
     private String filepath1;
@@ -21,9 +21,15 @@ public class App {
     @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
     private String format = "stylish";
 
+    @Override
+    public String call() throws Exception {
+        String dif = Differ.generate(filepath1, filepath2);
+        System.out.println(dif);
+        return dif;
+    }
+
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new App()).execute(args);
-        System.exit(exitCode);
-        System.out.println("Hello, world!");
+            int exitCode = new CommandLine(new App()).execute(args);
+            System.exit(exitCode);
     }
 }
