c301c8b5d6778a50a1dca5a85168e72286156d81
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-06-30 01:07:49 +0500

    fix DifferTest.java

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
+import java.net.URL;
 import java.nio.file.Paths;
 import java.util.Objects;
 
@@ -10,10 +11,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DifferTest {
     @Test
     public void testGenerate() throws Exception {
-        String filepath1 = String.valueOf(Paths.get(Objects.requireNonNull(getClass().getResource("/testfile1.json")).getPath()));
-        String filepath2 = String.valueOf(Paths.get(Objects.requireNonNull(getClass().getResource("/testfile2.json")).getPath()));
+        URL resourceUrl1 = getClass().getResource("/testfile1.json");
+        Objects.requireNonNull(resourceUrl1, "Resource not found: testfile1.json");
+        String filePath1 = Paths.get(resourceUrl1.getPath()).toString();
 
-        var actual = Differ.generate(filepath1, filepath2);
+        URL resourceUrl2 = getClass().getResource("/testfile2.json");
+        Objects.requireNonNull(resourceUrl2, "Resource not found: testfile2.json");
+        String filePath2 = Paths.get(resourceUrl2.getPath()).toString();
+
+        var actual = Differ.generate(filePath1, filePath2);
         String expected = """
                 {
                 - follow: false
