66bb6bfa910e81de99b3ea8ed730121208461fe0
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-06-30 20:42:39 +0500

    update Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,23 +1,15 @@
 package hexlet.code;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public class Differ {
     public static String generate(String filePath1, String filePath2) throws Exception {
-
-        Map<String, String> map1 = getData(filePath1);
-        Map<String, String> map2 = getData(filePath2);
-
+        Map<String, String> map1 = Parser.getData(filePath1);
+        Map<String, String> map2 = Parser.getData(filePath2);
         List<String> keys = Stream.concat(
                 map1.keySet().stream(),
                 map2.keySet().stream()
@@ -41,17 +33,6 @@ public class Differ {
                 result.add("+ " + key + ": " + map2.get(key));
             }
         }
-
         return result.stream().collect(Collectors.joining("\n", "{\n", "\n}"));
     }
-
-    private static Map<String, String> getData(String filePath) throws Exception {
-        Path path = Paths.get(filePath).toAbsolutePath().normalize();
-        if (!Files.exists(path)) {
-            throw new Exception("File " + filePath + " not found");
-        }
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(path.toFile(), new TypeReference<>() { });
-    }
-
 }
--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -2,24 +2,38 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
-import java.net.URL;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DifferTest {
     @Test
-    public void testGenerate() throws Exception {
-        URL resourceUrl1 = getClass().getResource("/testfile1.json");
-        Objects.requireNonNull(resourceUrl1, "Resource not found: testfile1.json");
-        String filePath1 = Paths.get(resourceUrl1.getPath()).toString();
+    public void testGenerateJson() throws Exception {
+        Path resourceDir = Paths.get("src", "test", "resources");
+        String testFilePath1 = resourceDir.resolve("testfile1.json").toAbsolutePath().toString();
+        String testFilePath2 = resourceDir.resolve("testfile2.json").toAbsolutePath().toString();
 
-        URL resourceUrl2 = getClass().getResource("/testfile2.json");
-        Objects.requireNonNull(resourceUrl2, "Resource not found: testfile2.json");
-        String filePath2 = Paths.get(resourceUrl2.getPath()).toString();
+        var actual = Differ.generate(testFilePath1, testFilePath2);
+        String expected = """
+                {
+                - follow: false
+                  host: hexlet.io
+                - proxy: 123.234.53.22
+                - timeout: 50
+                + timeout: 20
+                + verbose: true
+                }""";
+        assertEquals(expected, actual);
+    }
 
-        var actual = Differ.generate(filePath1, filePath2);
+    @Test
+    public void testGenerateYaml() throws Exception {
+        Path resourceDir = Paths.get("src", "test", "resources");
+        String testFilePath5 = resourceDir.resolve("testfile5.yml").toString();
+        String testFilePath6 = resourceDir.resolve("testfile6.yml").toString();
+
+        var actual = Differ.generate(testFilePath5, testFilePath6);
         String expected = """
                 {
                 - follow: false
