4bb296b91ab3c7b740ecc973aaf593273bebc69a
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-07-08 15:13:50 +0500

    add interface Formatter.java

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -1,18 +1,7 @@
 package hexlet.code;
 
-import java.util.Map;
+import java.util.List;
 
-public class Formatter {
-    public static String stylish(String key, Map<String, String> oldMap, Map<String, String> newMap) {
-        if (!oldMap.containsKey(key)) {
-            return "  + " + key + ": " + newMap.get(key);
-        }
-        if (!newMap.containsKey(key)) {
-            return "  - " + key + ": " + oldMap.get(key);
-        }
-        if (oldMap.get(key).equals(newMap.get(key))) {
-            return " ".repeat(4) + key + ": " + oldMap.get(key);
-        }
-        return  "  - " + key + ": " + oldMap.get(key) + "\n  + " + key + ": " + newMap.get(key);
-    }
+public interface Formatter {
+    String format(List<Differ> diffs);
 }
