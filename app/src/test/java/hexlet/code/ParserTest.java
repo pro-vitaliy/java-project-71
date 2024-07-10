2bb499cca464939598a6f79d140f371b56b4c25a
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-07-10 23:21:14 +0500

    refactoring ParserTest

--- app/src/test/java/hexlet/code/ParserTest.java
+++ app/src/test/java/hexlet/code/ParserTest.java
@@ -2,55 +2,45 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ParserTest {
     @Test
-    public void testGetDataJson() throws Exception {
+    public void testGetDataJson() throws IOException {
         Path resourceDir = Paths.get("src", "test", "resources");
         Path testFilePath = resourceDir.resolve("testfile1.json").toAbsolutePath();
+        Map<String, Object> testContent = Parser.getData(testFilePath.toString());
 
-        Map<String, String> actual = Parser.getData(testFilePath.toString());
-        Map<String, String> expected = Map.of(
-                "nums1", "[1, 2, 3, 4]",
-                "obj1", "{nestedKey=value, isNested=true}",
-                "nums2", "[22, 33, 44, 55]",
-                "default", ""
-        );
+        var actual = testContent.get("obj1");
+        var expected = Map.of("nestedKey", "value", "isNested", true);
         assertEquals(expected, actual);
     }
 
     @Test
-    public void testGetDataYaml() throws Exception {
+    public void testGetDataYml() throws IOException {
         Path resourceDir = Paths.get("src", "test", "resources");
         Path testFilePath = resourceDir.resolve("testfile6.yml").toAbsolutePath();
+        Map<String, Object> testContent = Parser.getData(testFilePath.toString());
 
-        Map<String, String> actual = Parser.getData(testFilePath.toString());
-        Map<String, String> expected = Map.of(
-                "nums2", "[22, 33, 44, 55]",
-                "chars1", "[a, b, c]",
-                "nums1", "[1, 2, 3, 4]",
-                "chars2", "false",
-                "default", "null",
-                "arr", "[]"
-        );
+        var actual = testContent.get("nums2");
+        var expected = List.of(22, 33, 44, 55);
         assertEquals(expected, actual);
     }
 
     @Test
-    public void testGetDataNonExistent() {
+    public void testGetDataYaml() throws IOException {
         Path resourceDir = Paths.get("src", "test", "resources");
-        Path testFilePath = resourceDir.resolve("nonExistentFile.json").toAbsolutePath();
+        Path testFilePath = resourceDir.resolve("testfile3.yaml").toAbsolutePath();
+        Map<String, Object> testContent = Parser.getData(testFilePath.toString());
 
-        var thrown = catchThrowable(
-                () -> Parser.getData(testFilePath.toString())
-        );
-        assertThat(thrown).isInstanceOf(Exception.class);
+        var actual = testContent.get("id");
+        assertNull(actual);
     }
 }
