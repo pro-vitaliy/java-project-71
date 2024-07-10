bae9c7fde0467684ef762c49cfafe7640e63ded7
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-07-10 20:53:38 +0500

    Changed the parameter type in the Formatter interface from List<Differ> to List<Map<String, Object>>

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -1,7 +1,8 @@
 package hexlet.code;
 
 import java.util.List;
+import java.util.Map;
 
 public interface Formatter {
-    String format(List<Differ> diffs);
+    String format(List<Map<String, Object>> diffs);
 }
