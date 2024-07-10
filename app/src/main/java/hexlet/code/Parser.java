5a48a465d178060c8e486b0ccd0ae1a9fa689fc0
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-07-10 20:48:19 +0500

    refactoring: change value type in Map from String to Object

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -12,27 +12,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Parser {
-    public static Map<String, String> getData(String filePath) throws Exception {
+    public static Map<String, Object> getData(String filePath) throws Exception {
         Path path = Paths.get(filePath).toAbsolutePath().normalize();
         if (!Files.exists(path)) {
             throw new Exception("File " + filePath + " not found");
         }
         String fileExtension = FilenameUtils.getExtension(filePath);
-        var fileContentMap = new HashMap<String, Object>();
+        var fileData = new HashMap<String, Object>();
 
         ObjectMapper mapper;
         if (fileExtension.equals("json")) {
             mapper = new ObjectMapper();
-            fileContentMap = mapper.readValue(path.toFile(), new TypeReference<>() { });
+            fileData = mapper.readValue(path.toFile(), new TypeReference<>() { });
         }
         if (fileExtension.equals("yml")) {
             mapper = new YAMLMapper();
-            fileContentMap = mapper.readValue(path.toFile(), new TypeReference<>() { });
+            fileData = mapper.readValue(path.toFile(), new TypeReference<>() { });
         }
-        var dataMap = new HashMap<String, String>();
-        for (Map.Entry<String, Object> entry : fileContentMap.entrySet()) {
-            dataMap.put(entry.getKey(), String.valueOf(entry.getValue()));
-        }
-        return dataMap;
+        return fileData;
     }
 }
