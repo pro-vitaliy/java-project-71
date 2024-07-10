bb7411962402d6d65075485e891f01ea922e0e2b
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-07-10 20:58:22 +0500

    added Json formatter

---
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -0,0 +1,23 @@
+package hexlet.code.formatters;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hexlet.code.Formatter;
+import lombok.SneakyThrows;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Json implements Formatter {
+    public String format(List<Map<String, Object>> diffs) {
+        return diffs.stream()
+                .map(Json::toJsonString)
+                .collect(Collectors.joining(", "));
+    }
+
+    @SneakyThrows
+    private static String toJsonString(Map<String, Object> diff) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(diff);
+    }
+}
