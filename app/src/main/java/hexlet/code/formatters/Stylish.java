9b24157267e85906f67862c0f9fb1ebd66f27852
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-07-10 20:57:54 +0500

    refactoring Stylish to work with List<Map<String, Object>> instead of List<Differ>

--- app/src/main/java/hexlet/code/formatters/Stylish.java
+++ app/src/main/java/hexlet/code/formatters/Stylish.java
@@ -1,27 +1,30 @@
 package hexlet.code.formatters;
 
-import hexlet.code.Differ;
 import hexlet.code.Formatter;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Stylish implements Formatter {
     @Override
-    public String format(List<Differ> diffs) {
+    public String format(List<Map<String, Object>> diffs) {
         return diffs.stream()
                 .map(Stylish::generate)
                 .collect(Collectors.joining("\n  ", "{\n  ", "\n}"));
     }
 
-    private static String generate(Differ diff) {
-        var key = diff.getKey();
-        var info = diff.getDiffInfo();
+    private static String generate(Map<String, Object> diff) {
+        var key = diff.get("key").toString();
+        var info = diff.get("info").toString();
+        var value = diff.get("value") != null ? diff.get("value").toString() : "null";
+        var oldValue = diff.get("oldValue") != null ? diff.get("oldValue").toString() : "null";
+        var newValue = diff.get("newValue") != null ? diff.get("newValue").toString() : "null";
         return switch (info) {
-            case "added" -> "+ " + key + ": " + diff.getValue2();
-            case "removed" -> "- " + key + ": " + diff.getValue1();
-            case "modified" -> "- " + key + ": " + diff.getValue1() + "\n  + " + key + ": " + diff.getValue2();
-            case "not changed" -> "  " + key + ": " + diff.getValue1();
+            case "ADDED" -> "+ " + key + ": " + value;
+            case "DELETED" -> "- " + key + ": " + value;
+            case "CHANGED" -> "- " + key + ": " + oldValue + "\n  + " + key + ": " + newValue;
+            case "UNCHANGED" -> "  " + key + ": " + value;
             default -> throw new IllegalStateException("Unexpected value: " + info);
         };
     }
