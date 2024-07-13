6b65c8387fc32f43d8b0a5f66cea1b2d693b01d5
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-07-13 21:50:53 +0500

    refactoring ParserTest

--- app/src/test/java/hexlet/code/ParserTest.java
+++ app/src/test/java/hexlet/code/ParserTest.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -12,11 +13,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ParserTest {
+    private static Path testfile1Path;
+    private static Path testfile3Path;
+    private static Path testfile6Path;
+
+    @BeforeAll
+    public static void beforeAll() {
+        Path resourceDir = Paths.get("src", "test", "resources");
+
+        testfile1Path = resourceDir.resolve("testfile1.json").toAbsolutePath();
+        testfile3Path = resourceDir.resolve("testfile3.yaml").toAbsolutePath();
+        testfile6Path = resourceDir.resolve("testfile6.yml").toAbsolutePath();
+    }
+
     @Test
     public void testGetDataJson() throws IOException {
-        Path resourceDir = Paths.get("src", "test", "resources");
-        Path testFilePath = resourceDir.resolve("testfile1.json").toAbsolutePath();
-        Map<String, Object> testContent = Parser.getData(testFilePath.toString());
+        Map<String, Object> testContent = Parser.getData(testfile1Path.toString());
 
         var actual = testContent.get("obj1");
         var expected = Map.of("nestedKey", "value", "isNested", true);
@@ -25,9 +37,7 @@ public class ParserTest {
 
     @Test
     public void testGetDataYml() throws IOException {
-        Path resourceDir = Paths.get("src", "test", "resources");
-        Path testFilePath = resourceDir.resolve("testfile6.yml").toAbsolutePath();
-        Map<String, Object> testContent = Parser.getData(testFilePath.toString());
+        Map<String, Object> testContent = Parser.getData(testfile6Path.toString());
 
         var actual = testContent.get("nums2");
         var expected = List.of(22, 33, 44, 55);
@@ -36,9 +46,7 @@ public class ParserTest {
 
     @Test
     public void testGetDataYaml() throws IOException {
-        Path resourceDir = Paths.get("src", "test", "resources");
-        Path testFilePath = resourceDir.resolve("testfile3.yaml").toAbsolutePath();
-        Map<String, Object> testContent = Parser.getData(testFilePath.toString());
+        Map<String, Object> testContent = Parser.getData(testfile3Path.toString());
 
         var actual = testContent.get("id");
         assertNull(actual);
