6491c62a53433c5f0a3b114c405caa996b1a893d
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-07-14 14:03:38 +0500

    make class ComparatorTest final

--- app/src/test/java/hexlet/code/ComparatorTest.java
+++ app/src/test/java/hexlet/code/ComparatorTest.java
@@ -10,7 +10,7 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-public class ComparatorTest {
+public final class ComparatorTest {
     private Map<String, Object> data1;
     private Map<String, Object> data2;
 
