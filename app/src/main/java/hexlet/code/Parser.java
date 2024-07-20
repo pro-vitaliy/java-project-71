9976eb57de25a6b6bf813a03d1dcae94d45d4101
Vitaliy Protsenko <pro.vitaliy@icloud.com>
2024-07-20 20:28:34 +0500

    refactoring Parser.java

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -3,33 +3,23 @@ package hexlet.code;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
-import org.apache.commons.io.FilenameUtils;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Parser {
-    public static Map<String, Object> getData(String filePath) throws IOException {
-        Path path = Paths.get(filePath).toAbsolutePath().normalize();
-        if (!Files.exists(path)) {
-            throw new IOException("File " + filePath + " not found");
-        }
-        String fileExtension = FilenameUtils.getExtension(filePath);
+    public static Map<String, Object> getData(String fileContent, String fileExtension) throws Exception {
         var fileData = new HashMap<String, Object>();
 
         ObjectMapper mapper;
         if (fileExtension.equals("json")) {
             mapper = new ObjectMapper();
-            fileData = mapper.readValue(path.toFile(), new TypeReference<>() { });
+            fileData = mapper.readValue(fileContent, new TypeReference<>() { });
         } else if (fileExtension.equals("yml") || fileExtension.equals("yaml")) {
             mapper = new YAMLMapper();
-            fileData = mapper.readValue(path.toFile(), new TypeReference<>() { });
+            fileData = mapper.readValue(fileContent, new TypeReference<>() { });
         } else {
-            throw new IOException("Unexpected file extension: " + fileExtension);
+            throw new Exception("Unexpected file extension: " + fileExtension);
         }
         return fileData;
     }
